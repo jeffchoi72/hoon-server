b020f603f240a0e9eb5938a867f2d39594c8f19c
jeffchoi72 <ihello0720@gmail.com>
2019-05-12 12:24:37 +0900

    implements user local login  and local register

--- src/graphql/auth/auth.type.ts
+++ src/graphql/auth/auth.type.ts
@@ -1,13 +1,10 @@
 import { ObjectType, Field } from 'type-graphql';
 import { User } from '../user/user.type';
 
-// 토큰
-// 유저 정보
-
 @ObjectType()
 export class Auth {
   @Field()
-  token: string;
+  authToken: string;
 
   @Field()
   user: User;
